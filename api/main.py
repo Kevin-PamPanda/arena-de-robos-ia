c7d4c56af73647f474a6af381ba86dfeb68a3b31
Kevin-PamPanda <kvn.minervino@gmail.com>
2025-12-11 12:13:57 -0300

    fix: liberar CORS para frontend na Vercel

--- api/main.py
+++ api/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://arena-de-robos-ia.vercel.app/",
+        "https://arena-de-robos-ia.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
